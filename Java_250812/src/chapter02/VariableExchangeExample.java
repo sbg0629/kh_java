6212e4c148ae42e6f748402d51c86a8eaa2e02ca
sbg0629 <sbg0629@naver.com>
2025-08-12 19:28:36 +0900

    x, y 값 받아서 출력 하고 x랑 y값 변경

---
+++ Java_250812/src/chapter02/VariableExchangeExample.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+public class VariableExchangeExample {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 5;
+		System.out.println("x:"+x+" y:"+y);
+		
+//		x값을 임시저장
+		int temp = x;
+//		x를 y값으로 저장
+		x = y;
+		//임시 값을 y에 저장
+		y = temp;
+		System.out.println("x:"+x+" y:"+y);
+	}
+
+}
