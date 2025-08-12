f677d22bbd792c41dee90b50002b00789cad69b3
sbg0629 <sbg0629@naver.com>
2025-08-12 19:29:36 +0900

    변수를 연산해서 변수로 저장

---
+++ Java_250812/src/chapter02/VariablelinitializationExample.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class VariablelinitializationExample {
+	public static void main(String[] args) {
+		int value = 20;
+		int result = value +10; //변수를 연산해서 변수로 저장함
+		System.out.println(result);
+	}
+}
