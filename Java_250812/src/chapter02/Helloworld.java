9c0e2b71c9dae9a76a791f285f06bd31027161c4
sbg0629 <sbg0629@naver.com>
2025-08-12 19:11:04 +0900

    자바 시작 헬로우 월드 출력

---
+++ Java_250812/src/chapter02/Helloworld.java
@@ -0,0 +1,24 @@
+package chapter02;
+
+/*
+ * 작성일지 ": 2025-08-12 
+ * 작성자: 나.
+ * 내용 : Hellowrold 클래스로 문자열 출력 
+ * */
+// jvm: 자방 가상 기계  
+// jvm > public class를 찾ㅇ아감 1번 
+//jvm > main 메소드를 찾아감 2번
+public class Helloworld {
+	public static void main(String[] args) {
+//		String : 문자열 타입
+//		message: 변수명(명)
+//		String message; //변수 선언
+//		message = "헬로 월드!"; //변수 값 할당
+
+		String message = "헬로우우우우우우 월드!"; //변수 선언 + 초기화. 
+		System.out.println(message);
+		
+		message = "웨ㅔㅔㅔㅔㅔㅔㅔㄹㄹㄹㄹㄹㄹㄹㄹㅋㅋㅋㅋㅋㅋㅋ커ㅓㅓㅓㅓㅁㅁㅁㅁㅁㅁㅌㅌㅌㅌ투ㅜㅜㅜㅜㅜㅜㅜㅎㅎㅎ후ㅜㅜㅞㅔㅔㄹㄹ!"; //변수 선언 + 초기화.
+		System.out.println(message);
+	}
+}
