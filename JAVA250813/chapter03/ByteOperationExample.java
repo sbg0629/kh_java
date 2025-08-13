e1f07610b2f94640d252ee9f5cee1508cbaf18e5
sbg0629 <sbg0629@naver.com>
2025-08-13 18:34:56 +0900

    연산식 변수로 저장 하고 자동 변환 시킴

---
+++ JAVA250813/chapter03/ByteOperationExample.java
@@ -0,0 +1,19 @@
+package chapter03;
+
+public class ByteOperationExample {
+	public static void main(String[] args) {
+		//연산식을 변수로 저장
+		byte result1 = 10+20;
+		System.out.println(result1);
+		
+		byte x = 10;
+		byte y = 20;
+		
+//		byte result2 =x+y; 
+//		연산되어서 int로 자동 변환도이서 byte로 casting 함
+		
+		byte result2 =(byte)(x+y);
+		System.out.println(result2);
+	}
+
+}
