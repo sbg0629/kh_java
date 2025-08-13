fc28bfa16d9889f570d03e2cd0a4e7686d8cd85a
sbg0629 <sbg0629@naver.com>
2025-08-13 18:40:37 +0900

    0b ~~ 는 이진수

---
+++ JAVA250813/chapter03/IntergerLiteralExample.java
@@ -0,0 +1,21 @@
+package chapter03;
+
+public class IntergerLiteralExample {
+	public static void main(String[] args) {
+//		0b~: 이진수
+//		1*2의3승 + 0*2의 2승 +1*2의1승 +1*2의 0승
+		int var1 =0b1011;
+//		0~:8진수
+//		2*8의2승 + 0*8의1승 + 6*8의0승
+		int var2 = 0206;
+		
+		int var3 = 365;
+//		0x~ 16진수
+//		B*16의1승 + 3*16의0승 
+		int var4 = 0xB3;
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+	}
+}
