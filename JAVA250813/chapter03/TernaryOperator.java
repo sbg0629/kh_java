848e54b63846993fa1a304161de2417d0d9fbe22
sbg0629 <sbg0629@naver.com>
2025-08-13 18:45:35 +0900

    System.out.println("두 수의 차는 " +((a>b)?(a-b):(b-a)));

---
+++ JAVA250813/chapter03/TernaryOperator.java
@@ -0,0 +1,10 @@
+package chapter03;
+
+public class TernaryOperator {
+	public static void main(String[] args) {
+		int a = 3, b =5;
+//		a>b가 거짓이므로 b-a는 반환
+		System.out.println("두 수의 차는 " +((a>b)?(a-b):(b-a)));
+		
+	}
+}
