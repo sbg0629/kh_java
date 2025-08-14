b4fc75b4c2841bce7c20f8811ba5211ed628a7c6
sbg0629 <sbg0629@naver.com>
2025-08-14 18:39:52 +0900

    for문 하나 이용해서 구구단 3단 만들기

---
+++ J250814/src/chapter03/Ex3_2.java
@@ -0,0 +1,14 @@
+package chapter03;
+
+/*
+ * for문 사용해서 구구단 3단 만들기
+ * */
+public class Ex3_2 {
+	public static void main(String[] args) {
+		for(int i  = 1 ; i <=9 ; i++) {
+			System.out.println("3 X "+i+" = "+3*i);
+		}
+			
+	}
+}
+
