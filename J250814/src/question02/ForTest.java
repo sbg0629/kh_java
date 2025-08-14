1d2b8b4478b6c6b11d7cf3d2a118113cbf6941c5
sbg0629 <sbg0629@naver.com>
2025-08-14 19:03:08 +0900

    99까지 for문으로  짝수 합 출력

---
+++ J250814/src/question02/ForTest.java
@@ -0,0 +1,12 @@
+package question02;
+
+public class ForTest {
+public static void main(String[] args) {
+	int sum =0;
+	for (int i = 0; i<=99; i+=2) {
+		sum +=i;
+	}
+	System.out.println(sum);
+		
+}
+}
