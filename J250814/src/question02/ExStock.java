af82d0aa64d9502c4a516f156f070fea6ba6f4cb
sbg0629 <sbg0629@naver.com>
2025-08-14 19:01:17 +0900

    코스피 입 출력 상승장 횡보장 if else문

---
+++ J250814/src/question02/ExStock.java
@@ -0,0 +1,36 @@
+//package question02;
+//
+//import java.util.Scanner;
+//
+//public class ExStock {
+//public static void main(String[] args) {
+//	Scanner scanner = new Scanner(System.in);
+//	System.out.print("코스피 지수를 입력하세요");
+//	int a = scanner.nextInt(); //코스피지수
+//	System.out.print("환율을 입력하세요(1100~1200)");
+//	int b = scanner.nextInt();//환율
+//	
+//	if (b<1150) {
+//		if(a>=2000) {
+//			System.out.println("상승장");
+//		}else if(a>=2000) {
+//			System.out.println("횡보장");
+//		}else {
+//			System.out.println("하락장");
+//		}
+////	}else 
+////	{
+////			if(a>=2200) {
+////				System.out.println("상승장");			
+////			}
+////			else if (a>=2000) {
+////				System.out.println("횡보장");			
+//////			}
+//////		}else
+////		{
+////			System.out.println("하락장");
+////	}
+//////		scanner.close();
+////			
+//	}
+//}
