1cab0e09def32834d47082e08b67f6114d9864c1
sbg0629 <sbg0629@naver.com>
2025-08-14 18:40:56 +0900

    1~10까지 합계 for문으로 출력

---
+++ J250814/src/chapter03/Ex3_3.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+/*
+ * 1~10까지 합계
+ * */
+public class Ex3_3 {
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i  = 1 ; i <=10 ; i++) {
+			sum+=i;
+		}
+		System.out.println("총합 = " + sum);
+			
+	}
+}
+
