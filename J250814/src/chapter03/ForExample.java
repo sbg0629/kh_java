7d0ee73629f3f450a1dd50c3cb5e53dfb3480a84
sbg0629 <sbg0629@naver.com>
2025-08-14 18:41:21 +0900

    0~4까지 반복

---
+++ J250814/src/chapter03/ForExample.java
@@ -0,0 +1,11 @@
+package chapter03;
+
+public class ForExample {
+	public static void main(String[] args) {
+//		0~4까지 반복
+		for (int i = 0; i < 5; i++) {
+			System.out.println("i의값은?" +i);
+			
+		}
+	}
+}
