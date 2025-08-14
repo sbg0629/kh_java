f73947f904d9cef91658f937b6644cd08ee741c6
sbg0629 <sbg0629@naver.com>
2025-08-14 18:50:44 +0900

    369 일때 짝 출력

---
+++ J250814/src/question03/ContinueSamYukgu.java
@@ -0,0 +1,16 @@
+package question03;
+
+public class ContinueSamYukgu {
+	public static void main(String[] args) {
+		for(int i = 1 ; i<=9;i++) {
+			if(i %3==0) {
+				System.out.print(" 짝 ");
+			}else {
+				System.out.print(" "+i);
+				
+			}
+			
+		}
+		
+	}
+}
