68ce9772fa5366c52bc6e3195a8175d19a17b54e
sbg0629 <sbg0629@naver.com>
2025-08-14 19:05:26 +0900

    while문으로 99까지 짝수 합 출력

---
+++ J250814/src/question02/whileTest.java
@@ -0,0 +1,13 @@
+package question02;
+
+public class whileTest {
+public static void main(String[] args) {
+	int i = 0;
+	int sum = 0;
+	while (i <=99) {
+		sum +=i;
+		i+=2;
+	}
+	System.out.println(sum);
+}
+}
