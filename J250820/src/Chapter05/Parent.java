f1fe313ed40bc209fdf2bd6a452a9c3957fb31d6
sbg0629 <sbg0629@naver.com>
2025-08-20 18:43:18 +0900

    부모 메소드

---
+++ J250820/src/Chapter05/Parent.java
@@ -0,0 +1,12 @@
+package Chapter05;
+
+public class Parent {	
+	public String filed1;
+	public void method1() {
+		System.out.println("Parent-method1");
+	}
+	public void method2() {
+		System.out.println("Parent-method2");
+	}
+	
+}
