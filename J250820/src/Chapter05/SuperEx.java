54301b9d270b711f839c4e3d997fa508db534792
sbg0629 <sbg0629@naver.com>
2025-08-20 18:40:40 +0900

    super() 상속

---
+++ J250820/src/Chapter05/SuperEx.java
@@ -0,0 +1,45 @@
+package Chapter05;
+
+
+/*
+ * super()
+ * 를 사용
+ * */
+
+class Point2{
+	private int x, y; 
+	
+	public Point2() {
+		this.x=this.y=0;
+	}
+	
+	public Point2(int x, int y) {
+		this.x = x; this.y = y;
+	}
+
+	public void showPoint() {
+		System.out.println("("+x+","+y+")");
+	}
+	
+}
+class ColorPoint2 extends Point2{ 
+		private String color;
+		
+		public ColorPoint2(int x,int y ,String color) {
+			super(x,y);
+			this.color = color;
+		}
+		
+		public void showColorPoint2() {
+			System.out.print(color);
+			showPoint();
+		}
+	}
+
+
+public class SuperEx {
+	public static void main(String[] args) {
+	ColorPoint2 cp = new ColorPoint2(5, 6, "blue");
+	cp.showColorPoint2();
+}
+}
