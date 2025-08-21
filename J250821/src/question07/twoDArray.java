ec2e964b3f78a600967ca77b263e7214c05f6e5f
sbg0629 <sbg0629@naver.com>
2025-08-21 19:19:26 +0900

    랜덤 할수로 2중배열

---
+++ J250821/src/question07/twoDArray.java
@@ -0,0 +1,19 @@
+package question07;
+
+import java.util.Random;
+
+public class twoDArray {
+public static void main(String[] args) {
+	Random rd = new Random();
+	int[][] array = new int [4][4];
+	
+	for (int i = 0; i < array.length; i++) {
+		for (int j = 0; j < array[i].length; j++) {
+			array[i][j] = (int)(Math.random()*10)+1;
+			System.out.print(array[i][j]+" \t ");
+		}		
+		System.out.println();
+	}
+
+}
+}
